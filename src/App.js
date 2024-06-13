65fa16dea620d39b645cbb87455b1e6b81c31b24
saya514 <tokunosima8008@gmail.com>
2024-06-13 11:49:13 +0900

    welcompage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
+//libraryのインポート
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-function App() {
+// Componentのインポート
+import TransitionalScreen from './components/TransitionalScreen' // 開発者用Component
+import LoginComponent from './components/LoginComponent'
+import CreateAccount from './components/CreateAccount'
+import { CreateSchoolAccount } from './components/CreateAccount'
+import CategoryComponent from './components/CategoryComponent'
+import WelcomeComponent from './components/WelcomeComponent'
+import EntitySelection from './components/EntitySelection'
+import UniversityHome from './universityComponents/UniversityHome'
+import UniversitySettingsPage from './universityComponents/UniversitySettingsPage'
+import UserSettingsPage from './userComponents/UserSettingsPage'
+import ArticlePage from './userComponents/ArticlePage'
+import Article from './userComponents/Article'
+import UserHome from './userComponents/UserHome'
+//assetのインポート
+import './assets/scripts/animation'
+
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world.
-      </h1>
-    </div>
-  );
+    <Router>
+      <Routes>
+        {/* 確認用URL画面 */}
+        <Route path="/" element={<TransitionalScreen />} />
+
+        {/*ログイン*/}
+        <Route path="/login" element={<LoginComponent />} />
+        {/*Userアカウント新規作成*/}
+        <Route path="/createaccount" element={<CreateAccount />} />
+        {/*大学用アカウント新規作成*/}
+        <Route path="/createschoolaccount" element={<CreateSchoolAccount />} />
+        {/*カテゴリー登録*/}
+        <Route path="/category" element={<CategoryComponent />} />
+        {/*初期画面*/}
+        <Route path="/welcome" element={<WelcomeComponent />} />
+        {/*アカウント使用団体指定*/}
+        <Route path="/entitiyselection" element={<EntitySelection />} />
+
+        {/*---ログイン後(User)---*/}
+        {/*ホーム画面*/}
+        <Route path="/userhome" element={<UserHome />} />
+        {/*ユーザー設定*/}
+        <Route path="/usersettingpsage" element={<UserSettingsPage />} />
+        {/*アーティクルのVuew*/}
+        <Route path="/articlepage" element={<ArticlePage />} />
+        {/*アーティクルの要素(素材)*/}
+        <Route path="/article" element={<Article />} />
+
+        {/*---ログイン後(University)---*/}
+        {/*ホーム画面*/}
+        <Route path="/universityhome" element={<UniversityHome />} />
+        {/*ユーザー設定*/}
+        <Route
+          path="/universitysettingspage"
+          element={<UniversitySettingsPage />}
+        />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
