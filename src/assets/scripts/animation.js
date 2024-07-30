42fa1014a959b4a1a6ee0e52da9b62c9bcb914ae
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2024-07-30 22:29:41 +0900

    hoverの追加

--- src/assets/scripts/animation.js
+++ src/assets/scripts/animation.js
@@ -1,13 +1,34 @@
-// ノートを開く処理
-export function OpenNote(img) {  
-  const openPopupBtn = document.getElementById('noteImg');
-  openPopupBtn.addEventListener('click',() => {
-    
-  })
+import { useState, useCallback } from 'react'
 
-  return(img);
+// ノートを開く処理
+export function OpenNote(img) {
+  const openPopupBtn = document.getElementById('noteImg')
+  openPopupBtn.addEventListener('click', () => {})
+
+  return img
   // prompt()
-  
+
   // const test_div = document.getElementById("test_div");
   // console.log(test_div);
 }
+
+/*----------InputTextのFocusエフェクト---------- */
+const useFocusHover = () => {
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleFocus = useCallback(() => {
+    setIsHovered(true)
+  }, [])
+
+  const handleBlur = useCallback(() => {
+    setIsHovered(false)
+  }, [])
+
+  return {
+    isHovered,
+    handleFocus,
+    handleBlur,
+  }
+}
+
+export default useFocusHover
